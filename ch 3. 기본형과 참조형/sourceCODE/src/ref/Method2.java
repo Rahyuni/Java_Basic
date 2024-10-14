cbedca6366e2e9dad4f747e573a30d71ea1f0773
Rahyuni <hanrahyun@naver.com>
2024-10-14 23:47:14 +0900

    💡 [add] 참조 주소 확인용 주석 추가

--- ch 3. 기본형과 참조형/sourceCODE/src/ref/Method2.java
+++ ch 3. 기본형과 참조형/sourceCODE/src/ref/Method2.java
@@ -3,7 +3,9 @@ package ref;
 public class Method2 {
     public static void main(String[] args) {
         Student student1 = createStudent("폴리", 15, 90);
+        //System.out.println("student1 주소: " + student1);
         Student student2 = createStudent("라현", 23, 95);
+        //System.out.println("student2 주소: " + student2);
 
         printStudent(student1);
         printStudent(student2);
@@ -11,6 +13,7 @@ public class Method2 {
 
     static Student createStudent(String name, int age, int grade) {
         Student student = new Student();
+        //System.out.println("student 주소: " + student);
         student.name = name;
         student.age = age;
         student.grade = grade;
