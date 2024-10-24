3f3dc35afd1109897178f7ab4fbdb7322083ef2a
Rahyuni <hanrahyun@naver.com>
2024-10-25 00:16:30 +0900

    ✨ [add] ch.7 : 연습 문제 2번 - 3차 완료

--- ch 7. 접근제어자/sourceCODE/src/access/ex/ShoppingCart.java
+++ ch 7. 접근제어자/sourceCODE/src/access/ex/ShoppingCart.java
@@ -16,16 +16,18 @@ public class ShoppingCart {
 
     public void displayItems() {
         System.out.println("장바구니 상품 출력");
-        for (Item i : items) {
-            System.out.println("상품명: " + i.getName() + ", 합계: " + i.getTotalPrice());
+        for (int i = 0; i < itemCount; i++) {
+            Item item = items[i];
+            System.out.println("상품명: " + item.getName() + ", 합계: " + item.getTotalPrice());
         }
         System.out.println("전체 가격 합: " + calculateTotalPrice());
     }
 
     private int calculateTotalPrice() {
         int totalPrice = 0;
-        for (Item i : items) {
-            totalPrice += i.getTotalPrice();
+        for (int i = 0; i < itemCount; i++) {
+            Item item = items[i];
+            totalPrice += item.getTotalPrice();
         }
         return totalPrice;
     }
