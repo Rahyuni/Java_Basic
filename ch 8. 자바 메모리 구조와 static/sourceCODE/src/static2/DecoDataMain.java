83f5819b31675e4ba6a323cf51cefd65f15fbf7f
Rahyuni <hanrahyun@naver.com>
2024-10-27 00:08:33 +0900

    ✨ [add] ch.8 : static 메서드3 - static import

--- ch 8. 자바 메모리 구조와 static/sourceCODE/src/static2/DecoDataMain.java
+++ ch 8. 자바 메모리 구조와 static/sourceCODE/src/static2/DecoDataMain.java
@@ -1,9 +1,13 @@
 package static2;
 
+import static static2.DecoData.staticCall;
+
 public class DecoDataMain {
     public static void main(String[] args) {
         System.out.println("1. 정적 호출");
-        DecoData.staticCall();
+        staticCall();
+        staticCall();
+        staticCall();
 
         System.out.println("2. 인스턴스 호출1");
         DecoData data1 = new DecoData();
@@ -14,6 +18,6 @@ public class DecoDataMain {
         data2.instanceCall();
 
         // 직접 참조값 준 경우
-        DecoData.staticCall(data1);
+        staticCall(data1);
     }
 }
