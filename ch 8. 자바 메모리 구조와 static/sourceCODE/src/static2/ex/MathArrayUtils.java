332ae9fb1b0c0e41c0c9f73457ae17941a196b13
Rahyuni <hanrahyun@naver.com>
2024-10-27 23:19:33 +0900

    🐛 [modified] ch.8 : 연습 문제 2번 - 3차 완성

--- ch 8. 자바 메모리 구조와 static/sourceCODE/src/static2/ex/MathArrayUtils.java
+++ ch 8. 자바 메모리 구조와 static/sourceCODE/src/static2/ex/MathArrayUtils.java
@@ -4,42 +4,30 @@ public class MathArrayUtils {
 
     public static int sum(int[] array) {
         int num = 0;
-        for (int i = 0; i < array.length; i++) {
-            num += array[i];
+        for (int value : array) {
+            num += value;
         }
         return num;
     }
 
     public static double average(int[] array) {
-        int num = 0;
-        int returnNum = 0;
-        for (int i = 0; i < array.length; i++) {
-            num += array[i];
-        }
-        returnNum += num / array.length;
-        return returnNum;
+        return (double) sum(array) / array.length;
     }
 
     public static int min(int[] array) {
-        int min = 0;
-        min += array[0];
-        for (int i = 0; i < array.length; i++) {
-            if (min <= array[i]) {
-                return min;
-            } else {
-                min += array[i];
+        int min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (min > array[i]) {
+                min = array[i];
             }
         }
         return min;
     }
 
     public static int max(int[] array) {
-        int max = 0;
-        max += array[0];
-        for (int i = 0; i < array.length; i++) {
-            if (max > array[i]) {
-                return max;
-            } else {
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (max < array[i]) {
                 max = array[i];
             }
         }
